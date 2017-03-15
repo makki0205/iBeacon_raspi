76656f4ef2e8ad87ed289f825e8285bcbae24c33
katsuramaki taiki <llxo2_5oxll@icloud.com>
2017-03-15 13:18:36 +0900

    initial commit

---
+++ beacon.js
@@ -0,0 +1,15 @@
+Bleacon = require('bleacon');
+//
+var uuid = '2FB85DF6-FDC7-4191-BB46-53DBA9A14415';
+var major = 120;
+var minor = 50;
+var measuredPower = -59;
+
+Bleacon.startAdvertising(uuid, major, minor, measuredPower);
+
+// Bleacon = require('bleacon');
+// Bleacon.startScanning();
+//
+// Bleacon.on('discover', function(bleacon) {
+//    console.dir(bleacon);
+// });
